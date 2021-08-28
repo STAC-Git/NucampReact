8f30cddcc9b6fdf7c454ad3ed82fc3fff19b3b45
Steven T.A. Carter <steve.carter@gmail.com>
2021-08-27 23:03:49 -0700

    Components Part 2

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Directory from './components/DirectoryComponent';
 import './App.css';
+import { CAMPSITES } from './shared/campsites';
 
 class App extends Component {
+    constructor(props) { 
+        super(props);
+        this.state = {
+            campsites: CAMPSITES
+        };
+    }   
     render() {
         return (
             <div className="App">
@@ -12,7 +19,7 @@ class App extends Component {
                     <NavbarBrand href="/">NuCamp</NavbarBrand>
                 </div>
                 </Navbar>
-                <Directory />
+                <Directory campsites={this.state.campsites} />
             </div>
         );
     }
--- nucampsite/src/index.js
+++ nucampsite/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'typeface-lobster';
+import 'typeface-open-sans'
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
