a6a95c7b6e04b35f930c740287138991b85301ac
Steven T.A. Carter <steve.carter@gmail.com>
2021-08-30 16:14:24 -0700

    Presentational and Container Components

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -49,9 +49,11 @@ class CampsiteInfo extends Component {
        render() {  
         if (this.props.campsite) {
             return (
-                <div className="row" >
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+                <div className="container">
+                    <div className="row" >
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
             );
         } 
