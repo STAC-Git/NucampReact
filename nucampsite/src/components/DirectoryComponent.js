dc018e2bf0a4e1431556b9af2100c6a8605ec57c
Steven T.A. Carter <steve.carter@gmail.com>
2021-09-03 22:48:22 -0700

    React Router Parameters

--- nucampsite/src/components/DirectoryComponent.js
+++ nucampsite/src/components/DirectoryComponent.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, } from 'reactstrap' ;
+import { Link } from'react-router-dom';
 
 function RenderDirectoryItem({campsite}) {
     return(
         <Card>
-        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-        <CardImgOverlay>
-            <CardTitle>{campsite.name}</CardTitle>
-        </CardImgOverlay>
-    </Card>
+            <Link to={`/directory/${campsite.id}`}>
+            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+            <CardImgOverlay>
+                <CardTitle>{campsite.name}</CardTitle>
+            </CardImgOverlay>
+            </Link>
+        </Card>
     );
 }
 
