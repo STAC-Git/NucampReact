92b06563bee7eaec0a8183e9c700fccc66e442ec
Steven T.A. Carter <steve.carter@gmail.com>
2021-09-03 22:23:56 -0700

    Adding to the Single Page App

--- nucampsite/src/components/MainComponent.js
+++ nucampsite/src/components/MainComponent.js
@@ -4,8 +4,12 @@ import CampsiteInfo from './CampsiteInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 
 
 class Main extends Component {
@@ -13,6 +17,9 @@ class Main extends Component {
         super(props);
         this.state = {
             campsites: CAMPSITES,
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS,
         };
     }
 
@@ -20,7 +27,11 @@ class Main extends Component {
 
         const HomePage = () => {
             return (
-                <Home />
+                <Home 
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
             );
         }
         return (
@@ -29,7 +40,9 @@ class Main extends Component {
                 <Switch>
                     <Route path= '/home' component={HomePage} />
                     <Route exact path='/directory' render ={() => <Directory campsites={this.state.campsites} />} />
+                    <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
+                   
                 </Switch>
                 <Footer />
             </div>
