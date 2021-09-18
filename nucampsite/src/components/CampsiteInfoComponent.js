403c89e2ebc1cfba9468898206d1ca99a4cbd4a1
Steven T.A. Carter <steve.carter@gmail.com>
2021-09-17 19:35:24 -0700

    React Animation Components

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -6,6 +6,7 @@ import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -107,13 +108,19 @@ class CommentForm extends Component {
 function RenderCampsite({campsite}) {
               return (
                 <div className="col-md-5 m-1">
-                    <Card>
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <Card>
                     <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
-                        <CardBody>
-                            <CardText>{campsite.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
+                    <CardBody>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
+        </div>
             );
         }
 
@@ -121,17 +128,23 @@ function RenderCampsite({campsite}) {
         if (comments) {
             return(
                 <div className="col-md-5 m-1">
-                    <h4>Comments</h4>
-                    {comments.map(comment => {
-                        return(
-                            <div key={comment.id}>
-                                <p>
-                                {comment.text} <br />
-                                -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                                </p>                             
-                            </div>                        
-                        );
-                    })}
+                      <h4>Comments</h4>
+                <Stagger in>
+                    {
+                        comments.map(comment => {
+                            return (
+                                <Fade in key={comment.id}>
+                                    <div>
+                                        <p>
+                                            {comment.text}<br />
+                                            -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                                        </p>
+                                    </div>
+                                </Fade>
+                            );
+                        })
+                    }
+                </Stagger>
                     <CommentForm campsiteId={campsiteId} postComment={postComment} />
              </div>
              
--- nucampsite/src/components/HomeComponent.js
+++ nucampsite/src/components/HomeComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 import {Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { FadeTransform } from 'react-animation-components';
 
 function RenderCard ({item, isLoading, errMess }) {
     if (isLoading) {
@@ -12,13 +13,19 @@ function RenderCard ({item, isLoading, errMess }) {
     }
 
     return (
-        <Card>
-            <CardImg src={baseUrl + item.image} alt={item.name} />
-            <CardBody>
-                <CardTitle>{item.name}</CardTitle>
-                <CardText>{item.description}</CardText>
-            </CardBody>
-        </Card>
+        <FadeTransform
+            in
+            transformProps={{
+                exitTransform: 'scale(0.5) translateY(50%)'
+            }}>
+            <Card>
+                <CardImg src={baseUrl + item.image} alt={item.name} />
+                <CardBody>
+                    <CardTitle>{item.name}</CardTitle>
+                    <CardText>{item.description}</CardText>
+                </CardBody>
+            </Card>
+        </FadeTransform>
     );
 }
 
